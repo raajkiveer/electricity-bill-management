fef7e2db46b7ec3a02ea1fdbef95e531f5bb086b
raajkiveer <rkgstar03@gmail.com>
2026-09-13 16:09:54 +0530

    Update server.js
    for render so it can use render,s port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Database connection
 const db = mysql.createConnection({
@@ -309,6 +309,6 @@ app.post('/delete-consumer', (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
